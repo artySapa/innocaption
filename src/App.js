c77dcd5603a56ac351b1b205359ff9ec0d49b3dc
Artyom Sapa <artem.sapa@gmail.com>
2024-03-28 22:07:11 -0700

    listed the products out

--- src/App.js
+++ src/App.js
@@ -4,30 +4,41 @@ import {useState, useEffect} from 'react';
 import axios from "axios";
 
 function App() {
-// const [products, setProducts] = useState({});
-// fetch('https://dummyjson.com/products')
-//   .then(res => res.json())
-//   .then(console.log);
-  const [products, setProducts] = useState({});
+  const [res, setRes] = useState({});
+  const [products, setProducts] = useState([]);
   const [test,setTest] = useState(0);
-  const getAllProducts = () => {
-    axios
-      .get("https://dummyjson.com/products")
-      .then((response) => {
-        setProducts(response.data);
-        console.log(products);
-      })
-      .catch(console.error);
-  }
 
+  /* get a list of all products using axios */
+  const getAllProducts = async () => {
+    try {
+      const response = await axios.get("https://dummyjson.com/products");
+      setRes(response.data);
+      setProducts(response.data.products);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   useEffect(() => {
     getAllProducts();
   }, []);
-
+  /* got the list of all the products */
   return (
+    <>
     <div>
-      <button onClick={() => {setTest(test + 1)}}>{test}</button>
+      <button onClick={() => {getAllProducts(); console.log(products)}}>{test}</button>
     </div>
+    <div className='w-[130%]'>
+    {products.map((product, index) => {
+          // Convert timestamp to localized date and time string
+          //Add a component that would be cards for products
+          return (
+            <div> 
+              {product.title}
+            </div>
+          );
+        })} 
+    </div>
+    </>
   );
 }
 
